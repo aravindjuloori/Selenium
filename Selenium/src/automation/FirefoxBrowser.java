79a0e82c77b27d18b4cca0a4a5c856b0cd78009a
aravi <aravi@192.168.0.9>
2020-10-19 22:18:52 +0530

    Added IE driver file

--- Selenium/src/automation/FirefoxBrowser.java
+++ Selenium/src/automation/FirefoxBrowser.java
@@ -1,16 +1,21 @@
 package automation;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class FirefoxBrowser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Firefox
+		/*
 		System.setProperty("webdriver.gecko.driver", "D:\\softwares\\geckodriver\\geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
-		
+		*/
+		System.setProperty("webdriver.ie.driver", "D:\\softwares\\IEDriverServer\\IEDriverServer.exe");
+		WebDriver driver=new InternetExplorerDriver();
 		driver.get("https://www.google.com"); //It opens browser in chrome browser
 		driver.manage().window().maximize(); //It maximizes the browser
 		System.out.println(driver.getTitle()); //It grabs the page title
