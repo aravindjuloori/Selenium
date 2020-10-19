194972e738131929c87386612bfda3ced3c11677
aravi <aravi@192.168.0.9>
2020-10-19 19:17:00 +0530

    Added Steps to run the tests in  chrome browser "16. How to run tests in Google Chrome"

--- Selenium/src/automation/Demo.java
+++ Selenium/src/automation/Demo.java
@@ -6,12 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 
 	public static void main(String[] args) {
-		//Create Driver object . it is specific to each browser.
+
 		
-		//Webdriver is an interface which gives all methods for the automation.
+		/*Step 1:Create Driver object . it is specific to each browser. It drivers all the automation. Below is the object created for
+		chrome driver */
+		
+		//ChromeDriver driver=new ChromeDriver();
+		
+		//Step 2:
+		/*But here we are actually executing and implementing methods present in webdriver interface. once Driver object We will strictly implement 
+		the methods of web driver  */
+		/*WebDriver is an interface which gives all methods for the automation.so you have to give the web driver knowledge to the driver object so that it will
+		automate each and every ui in the browser to give that return type for that should be WebDriver */ 
+		
+		//WebDriver driver=new ChromeDriver();
+		
+		//Step 3: This single step is not sufficient bcoz browsers are third party tools they will not allow to invoke the browser
 		System.setProperty("webdriver.chrome.driver", "D:\\softwares\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com");
+		
 	}
 
 }
