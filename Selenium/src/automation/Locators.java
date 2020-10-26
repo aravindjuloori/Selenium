ff97cc06cee0bd9f17feb9c4828422fc4a1f2662
aravi <aravi@Aravind>
2020-10-26 16:10:50 +0530

    Commiting "locator =28 session"

---
+++ Selenium/src/automation/Locators.java
@@ -0,0 +1,23 @@
+package automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver","D:\\\\softwares\\\\chromedriver_win32\\\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("Selenium");
+		
+		
+		
+	}
+
+}
