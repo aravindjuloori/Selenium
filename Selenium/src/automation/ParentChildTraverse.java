7d8433e661f1051efd4034b5cfb7257edb1765e6
aravindjuloori <aravind.juloori@gmail.com>
2020-11-03 18:21:01 +0530

    Added parentandchild xpath java file

---
+++ Selenium/src/automation/ParentChildTraverse.java
@@ -0,0 +1,20 @@
+package automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ParentChildTraverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\\\softwares\\\\chromedriver_win32\\\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//div[@class='RNNXgb']/div/div[2]/input")).sendKeys("Selenium");
+		//driver.findElement(By.xpath(xpathExpression))
+	}
+
+}
