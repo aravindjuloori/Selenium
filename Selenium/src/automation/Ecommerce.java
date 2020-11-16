800f44fa6ff16d23581850be61a769195bd837ff
aravindjuloori <aravind.juloori@gmail.com>
2020-11-16 19:14:04 +0530

    Ecommerce java class file and jar files location changes

--- Selenium/src/automation/Ecommerce.java
+++ Selenium/src/automation/Ecommerce.java
@@ -1,20 +1,46 @@
 package automation;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Ecommerce {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+		int j=0;
 		System.setProperty("webdriver.chrome.driver","D:\\softwares\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		
+		//Expected list of items to be added to the cart
+		
+		String[] itemsNeeded= {"Cucumber","Brocolli","Beetroot","Carrot"};
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
 		
+		List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
+		
+		for(int i=0;i<products.size();i++) {
+			
+			String[] name=products.get(i).getText().split("-");
+			String formattedName=name[0].trim();
+			List itemsNeededList=Arrays.asList(itemsNeeded);
+			
+			if(itemsNeededList.contains(formattedName)) {
+				j++;
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+			
+				if(j==itemsNeeded.length) {
+					break;
+				}
+			}
+			
+		}
 		
 	}
 
--- Selenium/src/automation/Javaalerts.java
+++ Selenium/src/automation/Javaalerts.java
@@ -27,4 +27,5 @@ public class Javaalerts {
 		driver.switchTo().alert().dismiss();
 	}
 
+	
 }
