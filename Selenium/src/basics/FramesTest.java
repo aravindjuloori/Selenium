11babf7e2057b6568e83145aa2322c6c8267cb63
Aravind Juloori <julooriaravind@gmail.com>
2025-10-15 22:58:31 +0530

    code for Iframes,Window handle and action class

---
+++ Selenium/src/basics/FramesTest.java
@@ -0,0 +1,36 @@
+package basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FramesTest {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+	//System.out.println(driver.findElements(By.tagName("iframe")).size());
+		//driver.switchTo().frame(driver.findElement(By.cssSelector(".demo-frame")));
+		driver.switchTo().frame(0);
+		
+		Actions a=new Actions(driver);
+		WebElement source=driver.findElement(By.id("draggable"));
+		WebElement target=driver.findElement(By.id("droppable"));
+		a.dragAndDrop(source, target).build().perform();	
+		
+		driver.switchTo().defaultContent();
+		
+
+	}
+
+}
