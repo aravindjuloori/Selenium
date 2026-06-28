eb753da8a5f53f4170cb7d71715d7b160ddc8080
Aravind Juloori <julooriaravind@gmail.com>
2026-06-28 19:40:07 +0530

    Pushing the changes related to dependency methods and grouping(testNG)

--- Selenium/src/day44/SignupTests.java
+++ Selenium/src/day44/SignupTests.java
@@ -3,17 +3,17 @@ package day44;
 import org.testng.annotations.Test;
 
 public class SignupTests {
-	@Test(priority=1,groups= {"regression"})
+	@Test(priority = 1, groups = { "regression" })
 	public void signupbyEmail() {
 		System.out.println("This is signupbyEmail");
 	}
 
-	@Test(priority=2,groups= {"regression"})
+	@Test(priority = 2, groups = { "regression" })
 	public void signupByFacebook() {
 		System.out.println("This is signupByFacebook");
 	}
-	
-	@Test(priority=3,groups= {"regression"})
+
+	@Test(priority = 3, groups = { "regression" })
 	public void signupBytwitter() {
 		System.out.println("This issignupBytwitter");
 	}
