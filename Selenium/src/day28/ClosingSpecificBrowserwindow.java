671a48235b084d79b61c372762864d62d0098622
Aravind Juloori <julooriaravind@gmail.com>
2026-09-06 22:41:38 +0530

    Pushing navigational methods concepts

--- Selenium/src/day28/ClosingSpecificBrowserwindow.java
+++ Selenium/src/day28/ClosingSpecificBrowserwindow.java
@@ -13,32 +13,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ClosingSpecificBrowserwindow {
 
 	public static void main(String[] args) {
-		
-		ChromeOptions options=new ChromeOptions();
+
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
-		
-		
+
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		
+		WebDriver driver = new ChromeDriver();
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.manage().window().maximize();
-		
+
 		driver.findElement(By.id("visitUsTwo")).click();
-		
-		Set<String> windowIDs=driver.getWindowHandles();
-		
-		for(String winId:windowIDs) {
-			
-			String title=driver.switchTo().window(winId).getTitle();
+
+		Set<String> windowIDs = driver.getWindowHandles();
+
+		for (String winId : windowIDs) {
+
+			String title = driver.switchTo().window(winId).getTitle();
 			System.out.println(title);
-			
-			if(title.equals("Rahul Shetty Academy | QA Automation, Playwright, AI Testing & Online Training")) {
+
+			if (title.equals("Rahul Shetty Academy | QA Automation, Playwright, AI Testing & Online Training")) {
 				driver.close();
 			}
-			 
+
 		}
 	}
 
